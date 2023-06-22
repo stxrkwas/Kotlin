c0da2ca84f9a0b400f41051440f1c6c164d965dc
Malu⚡ <124840632+stxrkwas@users.noreply.github.com>
2023-06-22 11:41:19 -0300

    .

--- IntelliJ IDEA/June/Lista 8/Exercicio7.kt
+++ IntelliJ IDEA/June/Lista 8/Exercicio7.kt
@@ -1,6 +1,6 @@
 //Maria Luiza Passo Silva
 
-/*Exercício 7:Criar um programa em que você entrar com o dia e o mês de uma data e informar quantos dias se passaram desde o início do ano.
+/*Exercício 7: Criar um programa em que você entrar com o dia e o mês de uma data e informar quantos dias se passaram desde o início do ano.
 Esqueça a questão dos anos bissextos e considere sempre que um mês possui 30 dias.*/
 
 //Importação de classe
